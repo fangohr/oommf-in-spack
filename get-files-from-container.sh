007bf7943c58eff4bf08372418add8c5d60b0f44
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2021-09-01 18:57:09 +0200

    removing other unneeded files

--- get-files-from-container.sh
+++
@@ -1,4 +0,0 @@
-sudo docker create -ti --name dummy oommf-spack-which
-sudo docker cp dummy:/packages-before.txt packages-before.txt
-sudo docker cp dummy:/packages-after.txt packages-after.txt
-sudo docker rm -f dummy
