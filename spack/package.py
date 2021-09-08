4b9058f9995394cb0847537a3c0865f13c63d0bd
Hans Fangohr <fangohr@soton.ac.uk>
2021-09-08 15:08:08 +0200

    mark older versions as 'deprecated'
    This way, they will not show up to the casual users, but are prepared should we
    ever want to play with them.

--- spack/package.py
+++ spack/package.py
@@ -51,10 +51,13 @@ class Oommf(Package):
         url = "https://math.nist.gov/oommf/dist/oommf20a2_20200608-hotfix.tar.gz"
     )
 
-
-    version('20a1_20180930',     sha256='c871e0dbb1522c3c1314af6c084b90cdbe69fd869b55ac94443851b74f818ed2')
-    version('20170929a0',        sha256='3439d1c9e95cc7395bc2e2330bba8cf198585d1b350251ea8561c1554ff8c7fd')
-    version('20160930b1',        sha256='363006f549bb63a39564fafc18b52342a14c1c3769c214467a39f72a0c0be36b')
+    # Deprecated versions have never been tested with spack.
+    version('20a1_20180930', deprecated=True,
+            sha256='c871e0dbb1522c3c1314af6c084b90cdbe69fd869b55ac94443851b74f818ed2')
+    version('20170929a0', deprecated=True,
+            sha256='3439d1c9e95cc7395bc2e2330bba8cf198585d1b350251ea8561c1554ff8c7fd')
+    version('20160930b1', deprecated=True,
+            sha256='363006f549bb63a39564fafc18b52342a14c1c3769c214467a39f72a0c0be36b')
 
     depends_on("tk", type=("build", "link", "test", "run"))
     depends_on("tcl", type=("build", "link", "test", "run"))
